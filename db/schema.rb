f9c8b62e6936bc8868271d12751694bea484bc60
Olalekan Oladipupo <lakesida10@gmail.com>
2016-09-24 13:44:08 +0100

    Add Page controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924104852) do
+ActiveRecord::Schema.define(version: 20160924124228) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20160924104852) do
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
