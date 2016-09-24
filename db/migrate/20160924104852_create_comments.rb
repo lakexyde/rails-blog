4de6341f946284b58f5d655f7d47285f31c991bd
Olalekan Oladipupo <lakesida10@gmail.com>
2016-09-24 11:50:56 +0100

    Add Comment controller and model

---
+++ db/migrate/20160924104852_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :author
+      t.text :content
+      t.references :post, index: true, foreign_key: true
+      t.string :website
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923230615) do
+ActiveRecord::Schema.define(version: 20160924104852) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author"
+    t.text     "content"
+    t.integer  "post_id"
+    t.string   "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
