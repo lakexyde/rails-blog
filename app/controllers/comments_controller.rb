12c2dc8be6c5f7550115d32093f8ed0714169f92
Olalekan Oladipupo <lakesida10@gmail.com>
2016-09-24 12:20:46 +0100

    Add comments and associate with post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :find_post
+  
+  def create
+    @comment = @post.comments.create(comment_params)
+    redirect_to @post
+  end
+  
+  def destroy
+    @comment = @post.comments(params[:id])
+    @comment.destroy
+    redirect_to @post
+  end
+  private
+  
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+  def comment_params
+    params.require(:comment).permit(:author, :website, :content)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   validates_presence_of :title, limit: {minimum:5}
+  has_many :comments, dependent: :destroy
 end
